d87c6665180289628576247ac382fc71b3c562c2
m1n3sweeper12 <greyson.morgan363@gmail.com>
2026-01-29 11:28:11 -0500

    Initial Commit

---
+++ CSC308CalculatorApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CSC308CalculatorApp
+//
+//  Created by Morgan, Ashley F. on 1/29/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ CSC308CalculatorAppTests/CSC308CalculatorAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  CSC308CalculatorAppTests.swift
+//  CSC308CalculatorAppTests
+//
+//  Created by Morgan, Ashley F. on 1/29/26.
+//
+
+import Testing
+@testable import CSC308CalculatorApp
+
+struct CSC308CalculatorAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
