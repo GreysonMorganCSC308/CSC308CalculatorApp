2e6bca01149e5f26826fb4c6223c6410c620ee6c
m1n3sweeper12 <greyson.morgan363@gmail.com>
2026-01-29 11:58:25 -0500

    Added calculator functions

--- CSC308CalculatorApp/ViewController.swift
+++ CSC308CalculatorApp/ViewController.swift
@@ -8,7 +8,59 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var op1: UITextField!
+    @IBOutlet weak var op2: UITextField!
+    @IBOutlet weak var displayLabel: UILabel!
+    @IBOutlet weak var operButton: UIButton!
+    
+    @IBAction func operButton(_ sender: Any) {
+        let actionSheet = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
+        let plusAction = UIAlertAction(title: "+", style: .default) { (_) in
+            self.operButton.setTitle("+", for: .normal)
+        }
+        let minusAction = UIAlertAction(title: "-", style: .default) { (_) in
+            self.operButton.setTitle("-", for: .normal)
+        }
+        let timesAction = UIAlertAction(title: "*", style: .default) { (_) in
+            self.operButton.setTitle("*", for: .normal)
+        }
+        let divideAction = UIAlertAction(title: "/", style: .default) { (_) in
+            self.operButton.setTitle("/", for: .normal)
+        }
+        
+        actionSheet.addAction(plusAction)
+        actionSheet.addAction(minusAction)
+        actionSheet.addAction(timesAction)
+        actionSheet.addAction(divideAction)
+        present(actionSheet, animated: true, completion: nil)
+    }
+    
+    
+    @IBAction func calculateButton(_ sender: Any) {
+        let op1Value = Double(op1.text ?? "") ?? 0
+        let op2Value = Double(op2.text ?? "") ?? 0
+        var result: Double = 0
+        switch operButton.titleLabel?.text ?? "" {
+        case "+":
+            result = op1Value + op2Value
+            break
+        case "-":
+            result = op1Value - op2Value
+            break
+        case "*":
+            result = op1Value * op2Value
+            break
+        case "/":
+            result = op1Value / op2Value
+            break
+        default:
+            break
+        }
+        
+        self.displayLabel.text = String(result)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
