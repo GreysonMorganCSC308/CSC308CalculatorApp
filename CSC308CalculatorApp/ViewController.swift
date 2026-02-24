bff0f1069f9103bb1fde857a3052374327709ca5
m1n3sweeper12 <greyson.morgan363@gmail.com>
2026-02-24 12:20:54 -0500

    classwork

--- CSC308CalculatorApp/ViewController.swift
+++ CSC308CalculatorApp/ViewController.swift
@@ -38,27 +38,56 @@ class ViewController: UIViewController {
     
     
     @IBAction func calculateButton(_ sender: Any) {
-        let op1Value = Double(op1.text ?? "") ?? 0
-        let op2Value = Double(op2.text ?? "") ?? 0
-        var result: Double = 0
-        switch operButton.titleLabel?.text ?? "" {
+        //let op1Value = Double(op1.text ?? "") ?? 0
+        //let op2Value = Double(op2.text ?? "") ?? 0
+        //var result: Double = 0
+        
+        guard let op1Value = op1.text, let a = Int(op1Value)else{
+            let alert = UIAlertController(title: "Error", message: "Please enter a number", preferredStyle: .alert)
+            let action = UIAlertAction(title: "OK", style: .default)
+            alert.addAction(action)
+            present(alert, animated: true)
+            return
+        }
+        guard let op2Value = op2.text, let b = Int(op2Value)else{
+            let alert = UIAlertController(title: "Error", message: "Please enter a number", preferredStyle: .alert)
+            let action = UIAlertAction(title: "OK", style: .default)
+            alert.addAction(action)
+            present(alert, animated: true)
+            return
+        }
+        guard let op = operButton.title(for: .normal) else{
+            let alert = UIAlertController(title: "Error", message: "Please select an operator", preferredStyle: .alert)
+            let action = UIAlertAction(title: "OK", style: .default)
+            alert.addAction(action)
+            present(alert, animated: true)
+            return
+        }
+        
+        var result: Int? = nil
+        
+        switch op {
         case "+":
-            result = op1Value + op2Value
+            result = a + b
             break
         case "-":
-            result = op1Value - op2Value
+            result = a - b
             break
         case "*":
-            result = op1Value * op2Value
+            result = a * b
             break
         case "/":
-            result = op1Value / op2Value
+            result = a / b
             break
         default:
+            let alert = UIAlertController(title: "Error", message: "Please select an operator", preferredStyle: .alert)
+            let action = UIAlertAction(title: "OK", style: .default)
+            alert.addAction(action)
+            present(alert, animated: true)
             break
         }
         
-        self.displayLabel.text = String(result)
+        displayLabel.text = "\(result)"
     }
     
     override func viewDidLoad() {
