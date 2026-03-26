11fe76d0ef943d2108b60e79bd1888bad9220072
m1n3sweeper12 <greyson.morgan363@gmail.com>
2026-03-26 11:58:44 -0400

    3/26 changes

---
+++ CSC308CalculatorApp/Model/Operator.swift
@@ -0,0 +1,29 @@
+//
+//  Operator.swift
+//  CSC308CalculatorApp
+//
+//  Created by Morgan, Ashley F. on 3/26/26.
+//
+
+import UIKit
+
+class Operator: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
